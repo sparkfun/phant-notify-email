50360fe9c566894f1178f70272610896d5591306
Todd Treece <toddtreece@gmail.com>
2014-05-08 08:01:46 -0400

    initial commit

---
+++ test/phant-notify-email_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var PhantNotify = require('../lib/phant-notify-email.js'),
+    notify = PhantNotify();
+
+exports.phantNotify = {
+
+  setUp: function(done) {
+    done();
+  },
+
+  tearDown: function(done) {
+    done();
+  },
+
+};
+
